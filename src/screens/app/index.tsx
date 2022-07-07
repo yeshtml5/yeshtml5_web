00acf4d8ad9b528e4f37eb357e47fb80c3f48fb8
wanhwi.son <yeshtml5@gmail.com>
2022-07-07 09:31:22 +0900

    updates

--- src/screens/app/index.tsx
+++ src/screens/app/index.tsx
@@ -1,9 +1,9 @@
 /**
  * @name App
  */
-import React from "react";
-import Routes from "../../../src/lib/routes";
+import React from 'react'
+import Routes from '../../../src/lib/routes'
 
 export default function Index() {
-    return <Routes />;
+  return <Routes />
 }
